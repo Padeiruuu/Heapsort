ff2e5d3826d53e1ddf8772ae1058c0458e50dd3a
Padeiruuu <126924113+Padeiruuu@users.noreply.github.com>
2024-03-26 16:48:38 -0300

    Update App.java

--- baeldung/src/main/java/com/baeldung/App.java
+++ baeldung/src/main/java/com/baeldung/App.java
@@ -1,21 +1,14 @@
 package com.baeldung;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {   
     public static boolean isMaxHeap(int[] a) {
         int n = a.length;
-        // Verifica se cada nó pai é maior ou igual que seus filhos
         for (int i = 0; i <= (n - 2) / 2; i++) {
             int leftChild = 2 * i + 1;
             int rightChild = 2 * i + 2;
-            // Verifica se o filho esquerdo existe e se o valor do pai é menor que o filho esquerdo
             if (leftChild < n && a[i] < a[leftChild])
                 return false;
-            // Verifica se o filho direito existe e se o valor do pai é menor que o filho direito
             if (rightChild < n && a[i] < a[rightChild])
                 return false;
         }
@@ -23,8 +16,8 @@ public class App
     }
 
     public static void main( String[] args ) {
-        int[] maxHeap = {100, 19, 36, 17, 100, 25, 1};
-        if (isMaxHeap(maxHeap))
+        int[] heap = {100, 19, 36, 17, 100, 25, 1};
+        if (isMaxHeap(heap))
             System.out.println("É um heap");
         else
             System.out.println("Não é um heap");    
